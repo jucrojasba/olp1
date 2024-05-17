4e671d2d0cf8fbc096d0fa608695786ecd54fa11
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-05-17 17:24:58 -0500

    Complete SideBar

--- olp1-fr/app/components/sidebar-menu/sidebar-menu.js
+++ olp1-fr/app/components/sidebar-menu/sidebar-menu.js
@@ -1,4 +1,11 @@
 import styles from "./sidebar-menu.css";
+import home_icon from "../../assets/imagenes/Home/home_icon.png";
+import forum_icon from "../../assets/imagenes/Home/icon_forum.png";
+import learning_icon from "../../assets/imagenes/Home/icon_cohete.png";
+import challenges_icon from "../../assets/imagenes/Home/icon_challenge.png";
+import html_icon from "../../assets/imagenes/Home/w3_html5-icon.svg";
+import css_icon from "../../assets/imagenes/Home/w3_css-icon.svg";
+import javascript_icon from "../../assets/imagenes/Home/javascript-vertical.svg";
 
 export function SidebarMenu(data = []) {
   const path = window.location.pathname;
@@ -12,26 +19,31 @@ export function SidebarMenu(data = []) {
 
   return `
     <aside class="${styles["sidebar-menu"]}">
-    <div id="usuario">
+    <div id="${styles["usuario"]}">
     <a href="/dashboard/profile"><img src="https://randomuser.me/api/portraits/men/75.jpg"></a>
     <h3>Nicolas Picon</h3>
     </div>
-    <div id="discover">
+    <div id="${styles["discover"]}">
     <h3>Discover</h3>
-    <button id="${"/dashboard"}" type="button">Home</button>
-    <button id="${"/dashboard/forum"}" type="button">Forum</button>
-    <button id="${"/dashboard/learning-path"}" type="button">Learning Path</button>
-    <button id="${"/dashboard/challenges"}" type="button">Challenges</button>
+    <div class="${styles["container"]}">
+    <button id="${"/dashboard"}" type="button"><img src="${home_icon}">Home</button>
+    <button id="${"/dashboard/forum"}" type="button"><img src="${forum_icon}">Forum</button>
+    <button id="${"/dashboard/learning-path"}" type="button"><img src="${learning_icon}">Learning Path</button>
+    <button id="${"/dashboard/challenges"}" type="button"><img src="${challenges_icon}">Challenges</button></div>
     </div>
-    <div id="courses">
+    <div id="${styles["courses"]}">
     <h3>My Courses</h3>
-    <button id="${"/dashboard/html"}" type="button">HTML</button>
-    <button id="${"/dashboard/css"}" type="button">CSS</button>
-    <button id="${"/dashboard/javascript"}" type="button">JAVASCRIPT</button>
+    <div class="${styles["container"]}">
+    <button id="${"/dashboard/html"}" type="button"><img src="${html_icon}">HTML</button>
+    <button id="${"/dashboard/css"}" type="button"><img src="${css_icon}">CSS</button>
+    <button id="${"/dashboard/javascript"}" type="button"><img src="${javascript_icon}">JAVASCRIPT</button>
     </div>
-    <div id="salida">
+    </div>
+    <div id="${styles["salida"]}">
+    <div class="${styles["container"]}">
     <button id="${"/dashboard/users"}" type="button">Users</button>
     <button id="logout" type="button">Logout</button>
+    </div>
     </div>
 
     <ul>
