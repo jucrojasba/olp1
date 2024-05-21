af1dbe6eeb2e69eb135d39601b1d16eae6a7e517
Juan Camilo Rojas <jucrojasba@gmail.com>
2024-05-21 00:52:22 -0500

    Add new styles in home

--- olp1-fr/app/scenes/private/home/home.js
+++ olp1-fr/app/scenes/private/home/home.js
@@ -1,5 +1,8 @@
 import styles from "./home.css";
-import css_icon from "../../../assets/imagenes/Home/sun.png";
+import sun from "../../../assets/imagenes/Home/sun.png";
+import planet1 from "../../../assets/imagenes/Home/planet1.png";
+import planet2 from "../../../assets/imagenes/Home/planet2.png";
+import planet3 from "../../../assets/imagenes/Home/planet3.png";
 
 export function HomeScene() {
   const pageContent = `
@@ -7,27 +10,17 @@ export function HomeScene() {
         <ul class="${styles["wrapper"]}">
         <div class="${styles["sun"]}">
           <div class="${styles["star"]}">
-          <img src="${css_icon}">
-          </div>
-        </div>
-        <div class="${styles["mercury"]}">
-          <div class="${styles["planet"]}">
-            <div class="${styles["shadow"]}"></div>
-          </div>
-        </div>
-        <div class="${styles["venus"]}">
-          <div class="${styles["planet"]}">
-            <div class="${styles["shadow"]}"></div>
+          <img src="${sun}">
           </div>
         </div>
         <div class="${styles["earth"]}">
-          <div class="${styles["planet"]}"><div class="${styles["shadow"]}"></div></div>
+          <div class="${styles["planet"]}"><img src="${planet1}"><div class="${styles["shadow"]}"></div></div>
         </div>
         <div class="${styles["mars"]}">
-          <div class="${styles["planet"]}"><div class="${styles["shadow"]}"></div></div>
+          <div class="${styles["planet"]}"><img src="${planet2}"><div class="${styles["shadow"]}"></div></div>
         </div>
         <div class="${styles["jupiter"]}">
-          <div class="${styles["planet"]}"><div class="${styles["shadow"]}"></div></div>
+          <div class="${styles["planet"]}"><img src="${planet3}"><div class="${styles["shadow"]}"></div></div>
         </div>
         </ul>
       </div>
