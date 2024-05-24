30e5c4634167f3da466e339127d44e82f99b7d59
samuelacevedo9307 <samuelacevedo9307@gmail.com>
2024-05-23 20:40:14 -0500

    Update HTML scene

--- olp1-fr/app/scenes/private/html/html.js
+++ olp1-fr/app/scenes/private/html/html.js
@@ -2,12 +2,24 @@ import styles from "./html.css";
 
 export function HtmlScene() {
   let pageContent = `
-    
+    <h2><span>H</span>yper<span>T</span>ext <span>M</span>arkup <span>L</span>anguage </h2>
+    <button id="create-module">Crear</button>
+    <section class="${styles.container}" id="modulos"></section>
     `;
   let logic = async () => {
     /* Boton Blanco en el sideBar */
     const $whiteButton = document.getElementById("/dashboard/html");
     $whiteButton.style = "background-color:white";
+    /* Mostrar Modulos */
+    const $modulos = document.getElementById("modulos");
+    const responseVerModulos = await fetch(
+      "http://localhost:4000/api/modules/1"
+    );
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      throw new Error(`Error ${response.status}: ${errorMessage}`);
+    }
+    const modulosdb = await response.json();
   };
   return {
     pageContent,
