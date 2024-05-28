675975ee3a0edab9e65820cf6d34a78e22478c6c
samuelacevedo9307 <samuelacevedo9307@gmail.com>
2024-05-27 19:21:39 -0500

    Update game scene with animations

--- olp1-fr/app/scenes/private/html/juego.js
+++ olp1-fr/app/scenes/private/html/juego.js
@@ -1,13 +1,13 @@
-import styles from "./juego.css";
 import asteroide from "../../../assets/imagenes/challenges/asteroide_central.png";
-import spaceBase from "../../../assets/imagenes/moduloHTML/spaceBase.png";
 import planetLife from "../../../assets/imagenes/moduloHTML/planetaHabitable.png";
+import spaceBase from "../../../assets/imagenes/moduloHTML/spaceBase.png";
 import spaceship1 from "../../../assets/imagenes/moduloHTML/spaceship1.png";
+import styles from "./juego.css";
 
 export function juegoScene() {
   const pageContent = `
-    <h2>Juego de Prueba: salvanos del asteroide con flexbox</h2>
-    <p>Misión: usa los controles para alejar el asteroide a la zona objetivo</p>
+    <h2>Juego de Prueba: salvanos del ovni con el asteroide flexbox</h2>
+    <p>Misión: Destruye <spam>el ovni</spam> usando los controles para mover el <span>asteroide</span></p>
     <div class=${styles["game-container"]}>
         <div class=${styles["controls"]}>
             <label for="justifyContent">justify-content:</label>
@@ -24,11 +24,14 @@ export function juegoScene() {
             </select>
         </div>
         <div class=${styles["image-container"]} id="image-container">
-            <img src="${asteroide}" class=${styles["target-image"]}>
+            <div id="modalBase" class=${styles["modal"]}></div>
+            <img src="${asteroide}" class=${styles["target-image"]} id="asteroide">
             <img src="${spaceBase}" id=${styles["spaceBase"]}>
             <img src="${planetLife}" id=${styles["planetLife"]}>
             <img src="${spaceship1}" id=${styles["spaceShip"]}>
             <div id="modal" class=${styles["modal"]}></div>
+            <div id="modalNave" class=${styles["modal"]}></div>
+            
         </div>
     </div>
     `;
@@ -40,7 +43,13 @@ export function juegoScene() {
     const alignItemsSelect = document.getElementById("alignItems");
     const imageContainer = document.getElementById("image-container");
     const $planetLife = document.getElementById(`${styles["planetLife"]}`);
+    const $spaceShip = document.getElementById(`${styles["spaceShip"]}`);
+    const $spaceBase = document.getElementById(`${styles["spaceBase"]}`);
     const $modal = document.getElementById("modal");
+    const $modalNave = document.getElementById("modalNave");
+    const $modalBase = document.getElementById("modalBase");
+    const $asteroide = document.getElementById("asteroide");
+
 
     // Función para actualizar los estilos de la imagen
     const updateStyles = () => {
@@ -48,6 +57,28 @@ export function juegoScene() {
       imageContainer.style.justifyContent = justifyContentSelect.value;
       imageContainer.style.alignItems = alignItemsSelect.value;
       //controladores
+      if (justifyContentSelect.value === "flex-start" &&
+        alignItemsSelect.value === "center"
+      ) {
+        $spaceShip.classList.add(`${styles["fly-away"]}`);
+        $modalNave.innerHTML = `<div>
+        <h2>Destruiste la nave!</h2>
+      </div>;`;
+      } else {
+        $spaceShip.classList.remove(`${styles["fly-away"]}`);
+        $modalNave.innerHTML = ``;
+      };
+      if (justifyContentSelect.value === "flex-end" &&
+        alignItemsSelect.value === "flex-end"
+      ) {
+        $spaceBase.classList.add(`${styles["vibrate"]}`);
+        $modalBase.innerHTML = `<div>
+        <h2>Destruiste la base!</h2>
+      </div>;`;
+      } else {
+        $spaceBase.classList.remove(`${styles["vibrate"]}`);
+        $modalBase.innerHTML = ``;
+      };
       if (
         justifyContentSelect.value === "center" &&
         (alignItemsSelect.value === "center" ||
@@ -57,11 +88,12 @@ export function juegoScene() {
         $modal.innerHTML = `<div>
         <h2>Destruiste el planeta!</h2>
       </div>;`;
-      $planetLife.classList.add(`${styles["vibrate"]}`);
+      $asteroide.classList.add(`${styles["vibrate-asteroide"]}`);
       } else {
         $planetLife.classList.remove(`${styles["vibrate"]}`);
+        $asteroide.classList.remove(`${styles["vibrate-asteroide"]}`);
         $modal.innerHTML = ``;
-      }
+      };
     };
 
     // Añadir event listeners a los selectores
