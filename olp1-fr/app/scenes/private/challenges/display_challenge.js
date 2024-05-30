a04bec09b2dafc6bc330726c70f45f0392efa877
Deybison Sepulveda <camimilo2014@gmail.com>
2024-05-30 13:07:37 -0500

    crecion de reto

--- olp1-fr/app/scenes/private/challenges/display_challenge.js
+++ olp1-fr/app/scenes/private/challenges/display_challenge.js
@@ -9,6 +9,13 @@ export function ChallengeDisplayScene() {
         <h2>HTML</h2>
     </div>
     <div class=${styles.container} >
+        <div id="modal" class="${styles.modal}">
+            <div class="${styles.modalContent}">
+                <h2>Bienvenido</h2>
+                <p>Presiona el botón para comenzar la animación</p>
+                <button id="startButton">Start</button>
+            </div>
+        </div>
         <div class="${styles.forms_container}">
             <h3>Reto HTML: forms</h3>
             <div>
@@ -44,9 +51,13 @@ export function ChallengeDisplayScene() {
             </div>
         </div>
         <div>
+            <div>
+                <h3>Time</h3>
+            </div>
             <img src="${asteroid}" id="${styles["asteroid"]}">
             <img src="${planetLife}" id=${styles["planetLife"]}>
         </div>
+        
     </div>
         `;
 
@@ -55,7 +66,24 @@ export function ChallengeDisplayScene() {
         const $whiteButton = document.getElementById("/dashboard/challenges");
         $whiteButton.style = "background-color:white";
 
-        
+        const $planetLife = document.getElementById(`${styles["planetLife"]}`);
+        const $asteroid = document.getElementById(`${styles["asteroid"]}`);
+        const questionOne = document.getElementById("question_one");
+        const questionTwo = document.getElementById("question_two");
+        const questionThree = document.getElementById("question_three");
+
+        //evento de inicio de reto
+        document.addEventListener('DOMContentLoaded', function() {
+            const modal = document.getElementById('modal');
+            const startButton = document.getElementById('startButton');
+            const $asteroid = document.getElementById(`${styles["asteroid"]}`);
+          
+            startButton.addEventListener('click', function() {
+              modal.style.display = 'none';
+              $asteroid.style.display = 'block';
+              $asteroid.style.animation = 'moverImagen 30s forwards';
+            });
+          });
     }
     return {
         pageContent,
